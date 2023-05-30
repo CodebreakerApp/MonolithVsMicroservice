6a1e2dc9c7ede07c76f4eb9e49ad8d7196cffaaa
Sebastian <sebastian@szvetecz.at>
2023-05-30 16:26:04 +0200

    Created bot-factory

---
+++ src/CodeBreaker.Backend/BotLogic/IBotFactory.cs
@@ -0,0 +1,7 @@
+﻿using CodeBreaker.Backend.Data.Models.Bots;
+
+namespace CodeBreaker.Backend.BotLogic;
+public interface IBotFactory
+{
+    Bot CreateBot(string name, BotFactoryArgumnets args);
+}
---
+++ src/CodeBreaker.Backend/Visitors/Extensions/BotVisitorExtensions.cs
@@ -0,0 +1,8 @@
+﻿using CodeBreaker.Backend.Visitors;
+
+namespace CodeBreaker.Backend.Data.Models.Bots;
+
+public static class BotVisitorExtensions
+{
+    public static string GetName(this Bot bot) => bot.Accept(new BotNameVisitor());
+}
