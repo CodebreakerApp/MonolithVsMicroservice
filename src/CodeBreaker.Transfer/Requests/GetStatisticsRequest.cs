888d089622932a582177dd08682e159d5fd30efe
Sebastian <sebastian@szvetecz.at>
2023-06-07 23:37:41 +0200

    Removed IParsable from GetStatisticsRequest

--- src/CodeBreaker.Transfer/Requests/GetStatisticsRequest.cs
+++ src/CodeBreaker.Transfer/Requests/GetStatisticsRequest.cs
@@ -1,22 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿namespace CodeBreaker.Transfer.Requests;
 
-namespace CodeBreaker.Transfer.Requests;
-
-public class GetStatisticsRequest : IParsable<GetStatisticsRequest>
+public class GetStatisticsRequest
 {
     public required DateTime From { get; init; }
 
     public required DateTime To { get; init; }
 
     public string? GameType { get; init; }
-
-    public static GetStatisticsRequest Parse(string s, IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out GetStatisticsRequest result)
-    {
-        throw new NotImplementedException();
-    }
 }
