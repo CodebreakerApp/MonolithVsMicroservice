6225b71f551a018673eb1fcef27bfd9850fceaef
Sebastian <sebastian@szvetecz.at>
2023-06-12 02:52:38 +0200

    Added AzureAppConfiguration

--- src/Services/Game/src/GameService/Program.cs
+++ src/Services/Game/src/GameService/Program.cs
@@ -1,4 +1,19 @@
+using Azure.Identity;
+using GameService.Extensions;
+
+DefaultAzureCredential azureCredential = new();
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    Uri endpoint = new(builder.Configuration.GetRequired("AzureAppConfigurationEndpoint"));
+    options.Connect(endpoint, azureCredential)
+        .Select("Shared:*")
+        .Select("Shared:*", builder.Environment.EnvironmentName)
+        .Select("GameService:*")
+        .Select("GameService:*", builder.Environment.EnvironmentName);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from the GameService!");
