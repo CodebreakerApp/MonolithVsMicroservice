399aba6ee384e293f129e5c5968f4c282968bad1
Sebastian <sebastian@szvetecz.at>
2023-06-13 08:31:43 +0200

    Registered transfer types in JsonSerializerContext

--- src/Services/Game/src/GameService/Serialization/ApiJsonSerializerContext.cs
+++ src/Services/Game/src/GameService/Serialization/ApiJsonSerializerContext.cs
@@ -1,6 +1,16 @@
 ﻿using System.Text.Json.Serialization;
+using Transfer = CodeBreaker.Services.Games.Transfer.Api;
 
 namespace GameService.Serialization;
 
-[JsonSerializable(typeof(string))] // TODO remove dummy
+[JsonSerializable(typeof(Transfer.Game))]
+[JsonSerializable(typeof(Transfer.GameType))]
+[JsonSerializable(typeof(Transfer.Requests.CreateGameRequest))]
+[JsonSerializable(typeof(Transfer.Requests.CreateMoveRequest))]
+[JsonSerializable(typeof(Transfer.Responses.CreateGameResponse))]
+[JsonSerializable(typeof(Transfer.Responses.CreateMoveResponse))]
+[JsonSerializable(typeof(Transfer.Responses.GetGameResponse))]
+[JsonSerializable(typeof(Transfer.Responses.GetGamesResponse))]
+[JsonSerializable(typeof(Transfer.Responses.GetGameTypeResponse))]
+[JsonSerializable(typeof(Transfer.Responses.GetGameTypesResponse))]
 internal partial class ApiJsonSerializerContext : JsonSerializerContext { }
