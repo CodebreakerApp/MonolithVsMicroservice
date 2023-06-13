cab5b435fd59d021cfb57040775332c3c82850a5
Sebastian <sebastian@szvetecz.at>
2023-06-13 20:54:29 +0200

    Added AzureAppConfiguration

--- src/Services/Report/src/ReportService/Program.cs
+++ src/Services/Report/src/ReportService/Program.cs
@@ -1,4 +1,19 @@
+using Azure.Identity;
+using CodeBreaker.Services.Report.Extensions;
+
+DefaultAzureCredential azureCredential = new ();
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    Uri endpoint = new(builder.Configuration.GetRequired("AzureAppConfigurationEndpoint"));
+    options.Connect(endpoint, azureCredential)
+        .Select("Shared*")
+        .Select("Shared*", builder.Environment.EnvironmentName)
+        .Select("ReportService*")
+        .Select("ReportService*", builder.Environment.EnvironmentName);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from the report service!");
