299f8eb8bbf1fdd0abe736d27234fea087386ad1
Sebastian <sebastian@szvetecz.at>
2023-06-13 08:31:06 +0200

    Added transfer-mapping

---
+++ src/Services/Game/src/GameService/Mapping/FieldTransferMappingVisitor.cs
@@ -0,0 +1,19 @@
+﻿using CodeBreaker.Services.Games.Data.Models.Fields;
+
+namespace CodeBreaker.Services.Games.Mapping;
+
+internal class FieldTransferMappingVisitor : IFieldVisitor<Transfer.Api.Field>
+{
+    public Transfer.Api.Field Visit(ColorField field) =>
+        new Transfer.Api.Field()
+        {
+            Color = Enum.GetName(field.Color)
+        };
+
+    public Transfer.Api.Field Visit(ColorShapeField field) =>
+        new Transfer.Api.Field()
+        {
+            Color = Enum.GetName(field.Color),
+            Shape = Enum.GetName(field.Shape)
+        };
+}
---
+++ src/Services/Game/src/GameService/Mapping/TransferMapping.cs
@@ -0,0 +1,52 @@
+﻿using CodeBreaker.Services.Games.Data.Models;
+using CodeBreaker.Services.Games.Data.Models.Fields;
+using CodeBreaker.Services.Games.Data.Models.GameTypes;
+using CodeBreaker.Services.Games.Data.Models.Visitors;
+using Riok.Mapperly.Abstractions;
+
+namespace CodeBreaker.Services.Games.Mapping;
+
+[Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName, EnabledConversions = MappingConversionType.All & ~MappingConversionType.ToStringMethod)]
+internal static partial class TransferMapping
+{
+    public static partial Transfer.Api.Game ToTransfer(this Game model);
+
+    public static partial Transfer.Api.GameWithCode ToTransferWithCode(this Game model);
+
+    public static partial Transfer.Api.Move ToTransfer(this Move move);
+
+    public static partial Move ToModel(this Transfer.Api.Move transfer);
+
+    public static Field ToModel(this Transfer.Api.Field transfer)
+    {
+        if (transfer.Shape != null && transfer.Color != null)
+            return new ColorShapeField(Enum.Parse<FieldColor>(transfer.Color), Enum.Parse<FieldShape>(transfer.Shape));
+
+        if (transfer.Color != null)
+            return new ColorField(Enum.Parse<FieldColor>(transfer.Color));
+
+        throw new InvalidOperationException("Invalid field");
+    }
+
+    public static IReadOnlyList<Field> ToModel(this IReadOnlyList<Transfer.Api.Field> transfer) =>
+        transfer.Select(field => field.ToModel()).ToList();
+
+    public static Transfer.Api.Field ToTransfer(this Field field) => field.Accept(new FieldTransferMappingVisitor());
+
+    public static Transfer.Api.GameType ToTransfer(this GameType gameType) =>
+        new()
+        {
+            Name = gameType.GetName(),
+            Holes = gameType.Holes,
+            MaxMoves = gameType.MaxMoves,
+            PossibleFields = gameType.PossibleFields.Select(x => x.ToTransfer()),
+        };
+
+    public static partial IReadOnlyList<Transfer.Api.GameType> ToTransfer(this IReadOnlyList<GameType> gameTypes);
+
+    public static string ToName(this GameType gameType) => gameType.GetName();
+
+    public static string ToTransfer(this KeyPeg keyPeg) => Enum.GetName(keyPeg) ?? throw new InvalidOperationException("The given keypeg value does not exist");
+
+    public static partial IReadOnlyList<string> ToTransfer(this IReadOnlyList<KeyPeg> keyPegs);
+}
