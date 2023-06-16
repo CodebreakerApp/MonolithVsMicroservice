11d2d7d18d35fdf86be8b4354113d85e562ff500
Sebastian <sebastian@szvetecz.at>
2023-06-16 17:16:16 +0200

    Adapted converters in game-service dbcontext

--- src/Services/Game/src/Data/DatabaseContexts/Configurations/GameConfiguration.cs
+++ src/Services/Game/src/Data/DatabaseContexts/Configurations/GameConfiguration.cs
@@ -2,6 +2,7 @@
 using CodeBreaker.Services.Games.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodeBreaker.Services.Games.Data.DatabaseContexts.Configurations;
 
@@ -10,7 +11,7 @@ internal class GameConfiguration : IEntityTypeConfiguration<Game>
     public void Configure(EntityTypeBuilder<Game> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.Property(x => x.Type).HasConversion<GameTypeNameConverter>(); // Store gameType by name
+        builder.Property(x => x.Type).HasConversion<GameTypeNameConverter>();
         builder.Property(x => x.Code).HasConversion<FieldsConverter>();
         builder.OwnsMany(x => x.Moves, builder =>
         {
@@ -19,6 +20,7 @@ internal class GameConfiguration : IEntityTypeConfiguration<Game>
             builder.Property(x => x.Fields).HasConversion<FieldsConverter>();
             builder.Property(x => x.KeyPegs).HasConversion<KeyPegsConverter>();
         });
+        builder.Property(x => x.State).HasConversion<EnumToStringConverter<GameState>>();
         builder.HasIndex(x => x.Start);
         builder.HasIndex(x => x.End);
         builder.HasIndex(x => x.Type);
--- src/Services/Game/src/Data/DatabaseContexts/Converters/KeyPegsConverter.cs
+++ src/Services/Game/src/Data/DatabaseContexts/Converters/KeyPegsConverter.cs
@@ -1,21 +1,25 @@
 ﻿using CodeBreaker.Services.Games.Data.Models;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace CodeBreaker.Services.Games.Data.DatabaseContexts.Converters;
 
 public class KeyPegsConverter : ValueConverter<IReadOnlyList<KeyPeg>?, string?>
 {
+    private static readonly JsonSerializerOptions _jsonOptions = new();
+
+    static KeyPegsConverter() =>
+        _jsonOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+
     public KeyPegsConverter() : base
     (
         keyPegs => keyPegs == null
             ? null
-            : string.Join('.', keyPegs.Select(keyPeg => Enum.GetName(keyPeg))),
+            : JsonSerializer.Serialize(keyPegs, _jsonOptions),
         keyPegsString => keyPegsString == null
             ? null
-            : keyPegsString
-            .Split('.', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-            .Select(keyPegName => Enum.Parse<KeyPeg>(keyPegName))
-            .ToArray()
+            : JsonSerializer.Deserialize<KeyPeg[]>(keyPegsString, _jsonOptions)
     )
     { }
 }
