bdfd08e94ec85bcaa9bd1c51bfeb002b2102e30b
Sebastian <sebastian@szvetecz.at>
2023-06-16 17:13:21 +0200

    Storing state as string with index

--- src/Services/Report/src/Data/DatabaseContexts/Configurations/GameConfiguration.cs
+++ src/Services/Report/src/Data/DatabaseContexts/Configurations/GameConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using CodeBreaker.Services.Report.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodeBreaker.Services.Report.Data.DatabaseContexts.Configurations;
 
@@ -10,8 +11,10 @@ internal class GameConfiguration : IEntityTypeConfiguration<Game>
     {
         builder.HasKey(x => x.Id);
         builder.HasMany(x => x.Moves).WithOne();
+        builder.Property(x => x.State).HasConversion<EnumToStringConverter<GameState>>();
         builder.HasIndex(x => x.Start);
         builder.HasIndex(x => x.End);
         builder.HasIndex(x => x.Type);
+        builder.HasIndex(x => x.State);
     }
 }
