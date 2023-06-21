fa0ac5c1b2d923fa86b8ae2be533c99b58917e77
Sebastian <sebastian@szvetecz.at>
2023-06-21 23:48:35 +0200

    Added possibility to run the report-message-service infinitely

--- src/Services/Report/src/MessageWorker/Services/MessageService.cs
+++ src/Services/Report/src/MessageWorker/Services/MessageService.cs
@@ -3,6 +3,7 @@ using CodeBreaker.Services.Games.Messaging.Transfer.Payloads;
 using CodeBreaker.Services.Report.Data.Repositories;
 using CodeBreaker.Services.Report.MessageWorker.Mapping;
 using CodeBreaker.Services.Report.MessageWorker.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace CodeBreaker.Services.Report.MessageWorker.Services;
@@ -15,11 +16,14 @@ internal class MessageService
 
     private readonly IGameRepository _gameRepository;
 
-    public MessageService(IMessageSubscriber messageSubscriber, IOptions<MessageServiceOptions> options, IGameRepository gameRepository)
+    private readonly ILogger _logger;
+
+    public MessageService(IMessageSubscriber messageSubscriber, IOptions<MessageServiceOptions> options, IGameRepository gameRepository, ILogger<MessageService> logger)
     {
         _messageSubscriber = messageSubscriber;
         _options = options;
         _gameRepository = gameRepository;
+        _logger = logger;
         messageSubscriber.OnGameCreatedAsync += OnGameCreatedCallbackAsync;
         messageSubscriber.OnMoveCreatedAsync += OnMoveCreatedCallbackAsync;
         messageSubscriber.OnGameEndedAsync += OnGameEndedCallbackAsync;
@@ -29,6 +33,9 @@ internal class MessageService
     {
         await _messageSubscriber.StartAsync(cancellationToken);
 
+        while (_options.Value.StopAfterSecondsOfNoMessage < 0) // A value < 0 means, that the service should never stop. But the option may change.
+            await Task.Delay(TimeSpan.FromMinutes(10), cancellationToken);
+
         while (_messageSubscriber.NoMessageDuration == null || _messageSubscriber.NoMessageDuration < TimeSpan.FromSeconds(_options.Value.StopAfterSecondsOfNoMessage))
             await Task.Delay(1000, cancellationToken);
 
@@ -37,19 +44,19 @@ internal class MessageService
 
     private async Task OnGameCreatedCallbackAsync(GameCreatedPayload payload, CancellationToken cancellationToken)
     {
-        Console.WriteLine("OnGameCreated"); // TODO Replace with logging
+        _logger.LogInformation("OnGameCreated");
         await _gameRepository.CreateAsync(payload.ToModel(), cancellationToken);
     }
 
     private async Task OnMoveCreatedCallbackAsync(MoveCreatedPayload payload, CancellationToken cancellationToken)
     {
-        Console.WriteLine("OnMoveCreated");
+        _logger.LogInformation("OnMoveCreated");
         await _gameRepository.AddMoveAsync(payload.GameId, payload.ToMoveModel(), cancellationToken);
     }
 
     private async Task OnGameEndedCallbackAsync(GameEndedPayload payload, CancellationToken cancellationToken)
     {
-        Console.WriteLine("OnGameEnded");
+        _logger.LogInformation("OnGameEnded");
         var game = await _gameRepository.GetAsync(payload.Id, cancellationToken);
         payload.ToModel(game);
         await _gameRepository.UpdateAsync(payload.Id, game, cancellationToken);
